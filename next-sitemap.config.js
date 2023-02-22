e000d71419ce40e0538c4be3ed173054b1c420ea
王志森 <im.wongch1sum@gmail.com>
2023-02-22 18:36:55 +0800

    new posts

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || 'https://www.xiaojun.im',
+  // siteUrl: process.env.SITE_URL || 'https://www.xiaojun.im',
+  siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 }
